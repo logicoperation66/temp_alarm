fa3c5d7c0fb01504766257a430b79cfcb4b05a07
Adam <adamwawrzyniak5@gmail.com>
2022-11-02 23:25:49 +0100

    Trying to send email, but i doesn't work

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import requests
 import re
-import pywhatkit
+import smtplib, ssl
+
+port = 465 # For ssl
+password = 'cl9s8Zm4'
+sender_email = "piecwawrzyniak@gmail.com"
+reciver_email = "adamwawrzyniak5@gmail.com"
+message = """Temperatura krytyczna"""
 
 
-s = requests.session()
-url = "http://192.168.0.11"
-response = s.get(url=url)
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login("piecwawrzyniak@gmail.com", password=password)
+    server.sendmail(sender_email, reciver_email, message)
 
 
-s = response.text
-temp = re.search(r'<span id="temperaturec">(.*?)</span>', s).group(1)
+def get_temp():
+    s = requests.session()
+    url = "http://192.168.0.11"
+    response = s.get(url=url)
+    html = response.text
+    temp = re.search(r'<span id="temperaturec">(.*?)</span>', html).group(1)
+    return temp
+
+print(get_temp)
+temp = get_temp()
 print(temp)
-
-if temp > '65':
-    pywhatkit.sendwhatmsg_instantly('+48510306985', 'testowa wiadomosc', 10)
-else:
-    pywhatkit.sendwhatmsg_instantly('+48510306985', 'testowa wiadomosc elsa', 10)
