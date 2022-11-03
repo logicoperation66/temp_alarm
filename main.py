a9c986a13fe0c59e7add154c1153177183e8c518
Adam <adamwawrzyniak5@gmail.com>
2022-11-03 22:04:01 +0100

    Add dotenv and email notification

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import requests
 import re
 import smtplib, ssl
+from dotenv import load_dotenv
+import os 
 
-port = 465 # For ssl
-password = 'cl9s8Zm4'
-sender_email = "piecwawrzyniak@gmail.com"
-reciver_email = "adamwawrzyniak5@gmail.com"
-message = """Temperatura krytyczna"""
+load_dotenv()
 
-
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("piecwawrzyniak@gmail.com", password=password)
-    server.sendmail(sender_email, reciver_email, message)
-
+port = 465
+password = os.getenv("PASSWORD")
+sender_email = os.getenv('ADMIN_EMAIL')
+reciver_email = os.getenv('RECIVER_EMAIL')
 
 def get_temp():
     s = requests.session()
@@ -23,7 +18,16 @@ def get_temp():
     html = response.text
     temp = re.search(r'<span id="temperaturec">(.*?)</span>', html).group(1)
     return temp
-
-print(get_temp)
+    
 temp = get_temp()
-print(temp)
+message = f"Temperature is high !!!{temp}"
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(f"{sender_email}", password=password)
+    server.sendmail(sender_email, reciver_email, message)
+
+
+temp = get_temp()
+
