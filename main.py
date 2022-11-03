775c1cad01fa60f7a722490475338f923eec1f96
Adam <adamwawrzyniak5@gmail.com>
2022-11-03 22:26:12 +0100

    Refactor, ready to upload on container

--- main.py
+++ main.py
@@ -6,28 +6,32 @@ import os
 
 load_dotenv()
 
-port = 465
+port = 465   # SMTP port
 password = os.getenv("PASSWORD")
 sender_email = os.getenv('ADMIN_EMAIL')
 reciver_email = os.getenv('RECIVER_EMAIL')
+url = os.getenv('URL')
 
-def get_temp():
+
+def get_temp(url):
     s = requests.session()
-    url = "http://192.168.0.11"
     response = s.get(url=url)
     html = response.text
     temp = re.search(r'<span id="temperaturec">(.*?)</span>', html).group(1)
+    s.close()
     return temp
-    
-temp = get_temp()
-message = f"Temperature is high !!!{temp}"
 
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login(f"{sender_email}", password=password)
-    server.sendmail(sender_email, reciver_email, message)
+def send_mail(text):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(f"{sender_email}", password=password)
+        server.sendmail(sender_email, reciver_email, text)
+    return 0
 
 
-temp = get_temp()
+if __name__ == "__main__":
+    temp = get_temp(url)
+    if float(temp) > 68:
+        massage = f"UWAGA!!! Temperatura pieca rosnie {temp}"
+        send_mail(massage)
 
